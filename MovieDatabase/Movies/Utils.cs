2b8e31137eea3ef6726f5d64f684d78ae52f3373
Christoffer Sladin <christoffer.sladin@hotmail.com>
2023-04-27 13:13:53 +0200

    added utils (unnessesary)  done with landingpage

---
+++ MovieDatabase/Movies/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Movies
+{
+    public class Utils
+    {
+        public string genre { get; set; }
+    }
+}
